2129af6ed0e95afdb872d5d8ccc09cb6c4856155
Zowie Erickson <zowie.erickson@gmail.com>
2022-03-31 14:13:04 -0400

    color scheme generating on input color value

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const colorInput = document.querySelector("#color-scheme-input")
+const colorSchemeSelect = document.querySelector("#color-scheme")
 const submit = document.querySelector("#generate_submit")
+const redBg = document.querySelector(".generator_color_bg.red")
+const app = document.querySelector("#generator_colors")
+
+function renderColorsHtml(data) {
+    console.log(data)
+    console.log(data.colors.length);
+    const colorsArr = data.colors;
+    const colorsLength = colorsArr.length;
+    let html = ''
+
+    for (let color of colorsArr) {
+        console.log(color.hex.value)
+        html += `
+        <div class="generator_color">
+            <div class="generator_color_bg" style="background:${color.hex.value}">
+                <p class="generator_color_content_mobile">${color.hex.value}</p>
+            </div>
+            <div class="generator_color_content"><p>${color.hex.value}</p></div>
+        </div>`
+    }
+    app.innerHTML = html
+}
 
 colorInput.addEventListener("change", watchColorPicker, false)
 
@@ -8,15 +31,21 @@ function watchColorPicker() {
       return colorHex;
   }
 
-console.log(colorInput)
-
-fetch(`https://www.thecolorapi.com/scheme?hex=${watchColorPicker}`)
-    .then(response => response.json())
-    .then(data => console.log(data))
-
-console.log(submit)
-
 submit.addEventListener("click", () => {
+    const colorHex = colorInput.value.substring(1);
+    const colorScheme = colorSchemeSelect.value;
+
+    const baseUrl = 'https://www.thecolorapi.com';
+    const endPoint = '/scheme'
+    const query = `?hex=${colorHex}&mode=${colorScheme}`
+    const url = `${baseUrl}${endPoint}${query}`
+
     console.log(watchColorPicker())
     document.querySelector(".test").innerHTML = watchColorPicker()
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        renderColorsHtml(data)
+    })
 })
